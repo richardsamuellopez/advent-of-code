a7ebf1ac0cbd6a3d6e5210f2ea80468469fd5c61
Richard Lopez <richardsamuellopez@gmail.com>
2020-12-03 23:20:00 -0500

    Day 3 part 2

--- day3/aocD3.py
+++ day3/aocD3.py
@@ -2,14 +2,13 @@
 from array import array
 
 print('Hello World!')
-def main():
+def main(right, down):
     map = []
-    right = 3
-    down = 1
     x = 0
     y = 0
     trees = 0
     repeatLength = 0
+    mapLength = 0
     f= open("map", "r")
     f1= f.readlines()
     for line in f1:
@@ -20,18 +19,24 @@ def main():
         map.append(array)
 
     repeatLength = len(array)
+    mapLength = len(map)
 
-    # print(map)
-    for row in map:
-        # print(row)
-        if (row[x] == '#'):
+    for y in range(0, mapLength, down):
+        if(map[y][x] == '#'):
             trees += 1
         x += right
         if x >= repeatLength:
             x = x-repeatLength
+        y += down
 
     print('The number of trees: ')
     print(trees)
+    return trees
 
 if __name__== "__main__":
-    main()
+    trees1 = main(1,1)
+    trees2 = main(3,1)
+    trees3 = main(5,1)
+    trees4 = main(7,1)
+    trees5 = main(1,2)
+    print("Total trees: ", trees1 * trees2 * trees3 * trees4 * trees5)
